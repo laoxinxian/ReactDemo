8063f19c1a80090ab650649ddecedca008821bb2
Alamo <muyeshenjie@qq.com>
2019-12-28 17:54:35 +0800

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 引入react库
+import React,{Component,Fragment} from 'react'
+// 引入css样式
+import './App.css'
+export default class App extends Component{
+
+  render(){
+    let buer = true
+    let obj = {
+      name:'Alamo',
+      skill:'Web',
+      size:'172'
+    }
+    return( <div>
+      
+      {/* 标签 */}
+      <h1>一个段落</h1>
+      {/* 三元表达式 */}
+      {buer?'中了哦！':'不会出现的'}
+      {/* 算法 */}
+      <h3>1+1哦={1+1}</h3>
+      {/* 渲染对象 */}
+    <h2>本周冠军{obj.name}</h2>
+    </div>)
+
+  }
 }
-
-export default App;
